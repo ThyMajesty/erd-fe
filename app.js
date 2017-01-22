f605b58814af4af259ed76dc1b736647da5732be
thymajesty <yesthymajesty@gmail.com>
2017-01-23 00:50:12 +0200

    c'mon

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const Server = require('./server.js')
 const port = (process.env.PORT || 8080)
 const app = Server.app()
@@ -5,13 +6,21 @@ const app = Server.app()
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
-const config = require('./webpack.dev.config.js')
+const proxyMiddleware = require('http-proxy-middleware');
+const config = require('./config/webpack.dev.js')
+const devConfig = config.devServer;
+config.output.path = __dirname + config.output.path
 const compiler = webpack(config)
+
+if (devConfig.proxy) {
+    Object.keys(devConfig.proxy).forEach((context) => {
+        app.use(proxyMiddleware(context, devConfig.proxy[context]));
+    });
+}
 
 app.use(webpackHotMiddleware(compiler))
 app.use(webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
+    noInfo: true
 }))
 
 app.listen(port)
