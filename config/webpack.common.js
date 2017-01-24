0bce5588a48b4b565f062012d44ec48eecc14b54
thymajesty <yesthymajesty@gmail.com>
2017-01-24 15:53:27 +0200

    asd

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -44,7 +44,7 @@ module.exports = {
                 test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
                 loader: 'url?limit=10000&minetype=application/font-woff'
             }, {
-                test: /\.woff2$/,
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
                 loader: 'url?limit=10000&minetype=application/font-woff'
             }, {
                 test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
